e13a4053d74cedaaf520bde3d5cc029cd6f36716
lorenzodelia125 <lorenzodelia125@gmail.com>
2022-08-10 16:40:57 +0200

    movie reviews solana client

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,75 +1,127 @@
-import { FC } from 'react';
-import { Movie } from '../models/Movie';
-import { useState } from 'react';
-import { Box, Button, FormControl, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Textarea } from '@chakra-ui/react';
+import { FC } from "react";
+import { Movie } from "../models/Movie";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Textarea,
+} from "@chakra-ui/react";
+import * as web3 from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useRouter } from "next/router";
 
-const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+const MOVIE_REVIEW_PROGRAM_ID = "Ab6tVs5bNytCtUA1JUPSLhuLGiJuVHGJaNFYbARYkwgS";
 
 export const Form: FC = () => {
-    const [title, setTitle] = useState('')
-    const [rating, setRating] = useState(0)
-    const [message, setMessage] = useState('')
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+  const router = useRouter();
 
-    const handleSubmit = (event: any) => {
-        event.preventDefault()
-        const movie = new Movie(title, rating, message)
-        handleTransactionSubmit(movie)
-    }
-
-    const handleTransactionSubmit = async (movie: Movie) => {
-        console.log(JSON.stringify(movie))
-    }
-
-    return (
-        <Box
-            p={4}
-            display={{ md: "flex" }}
-            maxWidth="32rem"
-            borderWidth={1}
-            margin={2}
-            justifyContent="center"
-        >
-            <form onSubmit={handleSubmit}>
-                <FormControl isRequired>
-                    <FormLabel color='gray.200'>
-                        Movie Title
-                        </FormLabel>
-                    <Input 
-                    id='title' 
-                    color='gray.400'
-                    onChange={event => setTitle(event.currentTarget.value)}
-                />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel color='gray.200'>
-                        Add your review
-                        </FormLabel>
-                    <Textarea 
-                        id='review' 
-                        color='gray.400'
-                        onChange={event => setMessage(event.currentTarget.value)}
-                    />
-                </FormControl>
-                <FormControl isRequired>
-                    <FormLabel color='gray.200'>
-                        Rating
-                        </FormLabel>
-                    <NumberInput 
-                        max={5} 
-                        min={1} 
-                        onChange={(valueString) => setRating(parseInt(valueString))}
-                    >
-                        <NumberInputField id='amount' color='gray.400' />
-                        <NumberInputStepper color='gray.400'>
-                            <NumberIncrementStepper />
-                            <NumberDecrementStepper />
-                        </NumberInputStepper>
-                    </NumberInput>
-                </FormControl>
-                <Button width="full" mt={4} type="submit">
-                    Submit Review
-                </Button>
-            </form>
-        </Box>
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const movie = new Movie(
+      event.target.title.value,
+      parseInt(event.target.amount.value),
+      event.target.review.value
     );
-}
+    handleTransactionSubmit(movie);
+  };
+
+  const handleTransactionSubmit = async (movie: Movie) => {
+    if (!connection || !publicKey) return;
+    const buffer = movie.serialize();
+    const transaction = new web3.Transaction();
+
+    const [pda] = await web3.PublicKey.findProgramAddress(
+      [publicKey.toBuffer(), new TextEncoder().encode(movie.title)],
+      new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID)
+    );
+
+    const instruction = new web3.TransactionInstruction({
+      keys: [
+        {
+          pubkey: publicKey,
+          isSigner: true,
+          isWritable: false,
+        },
+        {
+          pubkey: pda,
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: web3.SystemProgram.programId,
+          isSigner: false,
+          isWritable: false,
+        },
+      ],
+      programId: new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID),
+      data: buffer,
+    });
+
+    transaction.add(instruction);
+
+    try {
+      let txid = await sendTransaction(transaction, connection);
+      console.log(
+        `Transaction submitted: https://explorer.solana.com/tx/${txid}?cluster=devnet`
+      );
+
+      const latestBlockHash = await connection.getLatestBlockhash();
+
+      await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: txid,
+      });
+
+      router.reload();
+    } catch (e) {
+      alert(JSON.stringify(e));
+    }
+
+    console.log(JSON.stringify(movie));
+  };
+
+  return (
+    <Box
+      p={4}
+      display={{ md: "flex" }}
+      maxWidth="32rem"
+      borderWidth={1}
+      margin={2}
+      justifyContent="center"
+    >
+      <form onSubmit={handleSubmit}>
+        <FormControl isRequired>
+          <FormLabel color="gray.200">Movie Title</FormLabel>
+          <Input id="title" color="gray.400" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel color="gray.200">Add your review</FormLabel>
+          <Textarea id="review" color="gray.400" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel color="gray.200">Rating</FormLabel>
+          <NumberInput max={5} min={1}>
+            <NumberInputField id="amount" color="gray.400" />
+            <NumberInputStepper color="gray.400">
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
+        <Button width="full" mt={4} type="submit">
+          Submit Review
+        </Button>
+      </form>
+    </Box>
+  );
+};
--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,25 +1,46 @@
-import { Card } from './Card'
-import { FC, useEffect, useState } from 'react'
-import { Movie } from '../models/Movie'
+import { Card } from "./Card";
+import { FC, useEffect, useState } from "react";
+import { Movie } from "../models/Movie";
+import { useConnection } from "@solana/wallet-adapter-react";
+import { PublicKey, AccountInfo } from "@solana/web3.js";
 
-const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
+const MOVIE_REVIEW_PROGRAM_ID = "Ab6tVs5bNytCtUA1JUPSLhuLGiJuVHGJaNFYbARYkwgS";
+
+type MovieAccountState = {
+  pubkey: PublicKey;
+  account: AccountInfo<Buffer>;
+};
 
 export const MovieList: FC = () => {
-    const [movies, setMovies] = useState<Movie[]>([])
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const { connection } = useConnection();
 
-    useEffect(() => {
-        setMovies(Movie.mocks)
-    }, [])
-    
-    return (
-        <div>
-            {
-                movies.map((movie, i) => {
-                    return (
-                        <Card key={i} movie={movie} />
-                    )
-                })
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    (async () => {
+      const moviesAccounts: MovieAccountState[] =
+        await connection.getProgramAccounts(
+          new PublicKey(MOVIE_REVIEW_PROGRAM_ID)
+        );
+
+      const reviews: Movie[] = moviesAccounts
+        .filter((m) => m != null)
+        .map((m) => {
+          console.log(m);
+          const movie = Movie.deserialize(m.account.data)!;
+          return movie;
+        });
+
+      if (reviews) {
+        setMovies(reviews);
+      }
+    })();
+  }, [connection]);
+
+  return (
+    <div>
+      {movies.map((movie, i) => {
+        return <Card key={i} movie={movie} />;
+      })}
+    </div>
+  );
+};
